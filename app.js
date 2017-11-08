fa167875461b373981becf0ed4349d0e6792b3d5
Unknown <mj0857363@otc.edu>
2017-11-08 12:22:43 -0600

    loading attempt

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 $(function () {
 
     const vm = Vue({
+        el: '#things',
 
+        data: {
+            dogs: [],
+        },
 
+        methods: ({
 
-
-
+            loadStuff() {
+                this.$http.post('https://dog.ceo/api/breeds/list/').then(response => {
+                    this.dogs = response.body
+                });
+            },
+        })
     })
 })
